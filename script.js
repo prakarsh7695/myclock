0d0392f381d7c1b6bfaf7af73627aff9386bcccd
prakarsh7695 <65502337+prakarsh7695@users.noreply.github.com>
2020-08-06 18:50:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         setInterval(() => {
             a = new Date(); 
-            date = a.toLocaleDateString(undefined, options);
+           
             time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-            document.getElementById('time').innerHTML = time + "<br>on " + date;
+            document.getElementById('time').innerHTML = time;
         }, 1000);
